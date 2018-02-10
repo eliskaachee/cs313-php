8289aa3b921702a0665aa843b43c1049dedaa922
Eliska <eliska.achee@gmail.com>
2018-02-10 10:55:10 -0700

    initial commit for quote site

---
+++ web/quote_site/view_quotes.php
@@ -0,0 +1,27 @@
+<?php
+try
+{
+  $user = 'postgres';
+  $password = 'Crochet11';
+  $db = new PDO('pgsql:host=localhost;dbname=postgres', $user, $password);
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+foreach ($db->query('SELECT * FROM quote') as $row)
+{
+    echo 'quote_id: ' . $row['quote_id'];
+    echo 'quote_text: ' . $row['quote_text'];
+    echo 'date_created: ' . $row['date_created'];
+    echo 'vote_count: ' . $row['vote_count'];
+    echo 'picture_id: ' . $row['picture_id'];
+    echo 'author_id: ' . $row['author_id'];
+    echo 'person_id: ' . $row['person_id'];
+    echo '<br/>';
+}
+?>
