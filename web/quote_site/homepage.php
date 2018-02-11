bc94ee72fd7ef740407a31864e73c27ac78379df
Eliska <eliska.achee@gmail.com>
2018-02-10 17:20:48 -0700

    added quotes and search

---
+++ web/quote_site/homepage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Quotes</title>
+    <link rel="stylesheet" href="/cs313-php/web/css/quote_site.css">
+	</head>
+	<body>
+    <div class="page-wrapper homepage">
+		    <h1 class="box">Quotes</h1>
+        <a href="/cs313-php/web/quote_site/view_quotes.php">View Quotes</a>
+      </div>
+	</body>
+</html>
--- web/quote_site/view_quotes.php
+++ web/quote_site/view_quotes.php
@@ -1,27 +1,77 @@
-<?php
-try
-{
-  $user = 'postgres';
-  $password = 'Crochet11';
-  $db = new PDO('pgsql:host=localhost;port=5432;dbname=postgres', $user, $password);
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>View Quotes</title>
+		<link rel="stylesheet" href="/cs313-php/web/css/quote_site.css">
+	</head>
+	<body>
+    <div class="page-wrapper">
+      <div class="quote-column">
+    <?php
+      try
+      {
+        $user = 'postgres';
+        $password = 'Crochet11';
+        $db = new PDO('pgsql:host=localhost;port=5432;dbname=postgres', $user, $password);
+        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+      }
+      catch (PDOException $ex)
+      {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+      }
+      if(isset($_POST["search_category"]) && $_POST["search_category"] != 'None') {
+        echo '<h1>Quotes about ' . $_POST["search_category"] . '</h1>';
+        $quote_table = $db->query('SELECT * FROM quote AS q JOIN author AS a ON q.author_id = a.author_id JOIN person p ON q.person_id = p.person_id JOIN quote_to_category qc ON q.quote_id = qc.quote_id JOIN category c ON qc.category_id = c.category_id WHERE category_name = \'' . $_POST["search_category"] . '\'');
+      } else {
+        echo '<h1>Quotes</h1>';
+        $quote_table = $db->query('SELECT * FROM quote AS q JOIN author AS a ON q.author_id = a.author_id JOIN person p ON q.person_id = p.person_id JOIN quote_to_category qc ON q.quote_id = qc.quote_id JOIN category c ON qc.category_id = c.category_id');
+      }
+      while ($row = $quote_table->fetch(PDO::FETCH_ASSOC))
+      {
+        // echo $_POST["search_category"];
+        echo '<div class="quote box">';
+          echo '<div class="votes">Votes:' . $row['vote_count'] . "</div>";
+          echo '<div class="quote_text">';
+            echo '<p>"' . $row['quote_text'] . '" --' . $row['author_name'] . '</p>';
+            echo '<div class="quote_info">';
+              echo '<span class="username">Added By: ' . $row['username'] . '</span>';
+              echo 'Date Added: ' . $row['date_created'];
+            echo '</div>';
+          echo '</div>';
+        echo '</div>';
+      }
+    ?>
+  </div>
+  <div class="search-column box">
+    <p>Search by Category</p>
+    <form action="#" method="post">
+    <select name="search_category">
+      <?php
+      try
+      {
+        $user = 'postgres';
+        $password = 'Crochet11';
+        $db = new PDO('pgsql:host=localhost;port=5432;dbname=postgres', $user, $password);
+        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+      }
+      catch (PDOException $ex)
+      {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+      }
 
-
-foreach ($db->query('SELECT * FROM quote') as $row)
-{
-    echo 'quote_id: ' . $row['quote_id'];
-    echo 'quote_text: ' . $row['quote_text'];
-    echo 'date_created: ' . $row['date_created'];
-    echo 'vote_count: ' . $row['vote_count'];
-    echo 'picture_id: ' . $row['picture_id'];
-    echo 'author_id: ' . $row['author_id'];
-    echo 'person_id: ' . $row['person_id'];
-    echo '<br/>';
-}
-?>
+      $category_list = $db->query('SELECT * FROM category');
+      echo '<option value="None">None</option>';
+      while ($row = $category_list->fetch(PDO::FETCH_ASSOC))
+      {
+        echo '<option value="' . $row['category_name'] . '">' . $row['category_name'] . '</option>';
+      }
+      ?>
+    </select>
+    <input type="submit" value="Search">
+  </form>
+  </div>
+  </div>
+  </body>
+</html>
